339a740ce019103f5426119f883863fb99dac1ad
Thomas Hawley <thawley22@turing.edu>
2023-02-01 18:12:23 -0700

    Update practice exercises

---
+++ practice_independent_challenges/objects_instances_class/runner.rb
@@ -0,0 +1,7 @@
+require './unicorn'
+
+charlie = Unicorn.new('Charlie')
+# erin = Unicorn.new
+# dani = Unicorn.new
+require 'pry'; binding.pry
+
---
+++ practice_independent_challenges/objects_instances_class/unicorn.rb
@@ -0,0 +1,6 @@
+class Unicorn
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
