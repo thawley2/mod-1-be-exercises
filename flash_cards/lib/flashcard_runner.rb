7b6135698f6b814cdc4c9b05ab06ea3763967208
Thomas Hawley <thawley22@turing.edu>
2023-02-01 20:28:00 -0700

    Complete flashcard game

--- flash_cards/lib/flashcard_runner.rb
+++ flash_cards/lib/flashcard_runner.rb
@@ -2,12 +2,11 @@ require './card'
 require './deck'
 require './turn'
 require './round'
+require './card_generator'
 
 def start
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  deck = Deck.new([card_1, card_2, card_3])
+  cards = CardGenerator.new('cards.txt').cards
+  deck = Deck.new(cards)
   round = Round.new(deck)
 
   p "Welcome! You're playing with #{round.deck.count} cards."
