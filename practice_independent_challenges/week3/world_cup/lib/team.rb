d13967e304f2e5306c7667b03c879dca1bc4a902
Thomas Hawley <thawley22@turing.edu>
2023-02-14 20:42:51 -0700

    Create player class with basic attributes and features

---
+++ practice_independent_challenges/week3/world_cup/lib/team.rb
@@ -0,0 +1,19 @@
+class Team
+
+  attr_reader :country, :players
+  attr_writer :eliminated
+
+  def initialize(country)
+    @country = country
+    @eliminated = false
+    @players = []
+  end
+
+  def eliminated?
+    @eliminated
+  end
+
+  def add_player(player)
+    @players << player
+  end
+end
