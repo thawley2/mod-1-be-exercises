d14dbfb1f841973e3270370e7988766f224f112d
Thomas Hawley <thawley22@turing.edu>
2023-02-14 20:32:49 -0700

    test: Add before each variable

--- practice_independent_challenges/week3/world_cup/spec/player_spec.rb
+++ practice_independent_challenges/week3/world_cup/spec/player_spec.rb
@@ -2,22 +2,22 @@ require './lib/player'
 
 RSpec.describe Player do
   before(:each) do
-    player = Player.new({name: "Luka Modric", position: "midfielder"}) 
+    @player = Player.new({name: "Luka Modric", position: "midfielder"}) 
   end
   describe '#initialize' do
     it 'exists' do
 
-      expect(player).to be_instance_of(Player)
+      expect(@player).to be_instance_of(Player)
     end
 
     it 'has a name' do
    
-      expect(player.name).to eq('Luka Modric')
+      expect(@player.name).to eq('Luka Modric')
     end
 
     it 'has a position' do
 
-      expect(player.position).to eq('midfielder')
+      expect(@player.position).to eq('midfielder')
     end
   end
 end
