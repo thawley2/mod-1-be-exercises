dd316d0dd98fdef012e7eb41b543e66e39a90672
Thomas Hawley <thawley22@turing.edu>
2023-02-14 20:28:42 -0700

    test: Add test for initializing player

---
+++ practice_independent_challenges/week3/world_cup/spec/player_spec.rb
@@ -0,0 +1,23 @@
+require './lib/player'
+
+RSpec.describe Player do
+  before(:each) do
+    player = Player.new({name: "Luka Modric", position: "midfielder"}) 
+  end
+  describe '#initialize' do
+    it 'exists' do
+
+      expect(player).to be_instance_of(Player)
+    end
+
+    it 'has a name' do
+   
+      expect(player.name).to eq('Luka Modric')
+    end
+
+    it 'has a position' do
+
+      expect(player.position).to eq('midfielder')
+    end
+  end
+end
